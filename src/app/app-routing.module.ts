e31c6826229e071f9e549c43d925331d46eed6b9
Michael Iglesias <mike.iglesias11@gmail.com>
2017-11-11 12:33:55 -0500

    added ng2-bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -9,7 +10,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
+  imports: [
+    RouterModule.forRoot(routes),
+    NgbModule.forRoot()
+  ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
